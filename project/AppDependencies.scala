b1a6d8381436c9bdf17761bd45e7f489cb69f040
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-02-26 16:38:46 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     "org.apache.httpcomponents"   %  "httpmime"                   % "4.5.7",
     "io.megl"                     %% "play-json-extra"            % "2.4.3",
     "uk.gov.hmrc"                 %% "bootstrap-play-25"          % "4.9.0",
-    "uk.gov.hmrc"                 %% "govuk-template"             % "5.28.0-play-25",
+    "uk.gov.hmrc"                 %% "govuk-template"             % "5.29.0-play-25",
     "uk.gov.hmrc"                 %% "play-json-union-formatter"  % "1.5.0",
     "uk.gov.hmrc"                 %% "play-reactivemongo"         % "6.2.0",
     "uk.gov.hmrc"                 %% "play-scheduling"            % "5.4.0",

9e67139f581ec089a5059ae8719c933702a000a2
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-03-05 21:19:07 +0000

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "uk.gov.hmrc"                 %% "play-json-union-formatter"  % "1.5.0",
     "uk.gov.hmrc"                 %% "play-reactivemongo"         % "6.2.0",
     "uk.gov.hmrc"                 %% "play-scheduling"            % "5.4.0",
-    "uk.gov.hmrc"                 %% "play-ui"                    % "7.32.0-play-25"
+    "uk.gov.hmrc"                 %% "play-ui"                    % "7.33.0-play-25"
   )
 
   private lazy val scope = "test, it"
@@ -24,7 +24,7 @@ object AppDependencies {
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % scope,
     "org.scalatest"           %% "scalatest"                % "3.0.4"                 % scope,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"                 % scope,
-    "uk.gov.hmrc"             %% "hmrctest"                 % "3.5.0-play-25"         % scope,
+    "uk.gov.hmrc"             %% "hmrctest"                 % "3.6.0-play-25"         % scope,
     "uk.gov.hmrc"             %% "reactivemongo-test"       % "3.1.0"                 % scope,
     "uk.gov.hmrc"             %% "service-integration-test" % "0.5.0-play-25"         % scope
   )

0b9ef4fabb71b13dc192eee19e560cdcae448a74
Stefano Sgorlon <gokyo@users.noreply.github.com>
2019-04-04 19:00:23 +0100

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,14 @@ import sbt._
 
 object AppDependencies {
 
+  private val httpComponentsVersion = "4.5.8"
+
   val compile: Seq[ModuleID] = Seq(
-    "org.apache.httpcomponents"   %  "httpclient"                 % "4.5.7",
-    "org.apache.httpcomponents"   %  "httpmime"                   % "4.5.7",
+    "org.apache.httpcomponents"   %  "httpclient"                 % httpComponentsVersion,
+    "org.apache.httpcomponents"   %  "httpmime"                   % httpComponentsVersion,
     "io.megl"                     %% "play-json-extra"            % "2.4.3",
     "uk.gov.hmrc"                 %% "bootstrap-play-25"          % "4.10.0",
-    "uk.gov.hmrc"                 %% "govuk-template"             % "5.30.0-play-25",
+    "uk.gov.hmrc"                 %% "govuk-template"             % "5.31.0-play-25",
     "uk.gov.hmrc"                 %% "play-json-union-formatter"  % "1.5.0",
     "uk.gov.hmrc"                 %% "play-reactivemongo"         % "6.2.0",
     "uk.gov.hmrc"                 %% "play-scheduling"            % "5.4.0",
@@ -20,7 +22,7 @@ object AppDependencies {
   val test: Seq[ModuleID] = Seq(
     "com.github.tomakehurst"  %  "wiremock"                 % "2.20.0"                % scope,
     "com.typesafe.play"       %% "play-test"                % current                 % scope,
-    "org.mockito"             %  "mockito-core"             % "2.25.1"                % scope,
+    "org.mockito"             %  "mockito-core"             % "2.26.0"                % scope,
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % scope,
     "org.scalatest"           %% "scalatest"                % "3.0.4"                 % scope,
     "org.scalaj"              %% "scalaj-http"              % "2.4.1"                 % scope,

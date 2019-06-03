7663b0565b7fc66c06c3ac779aad9dc059d203b6
Ed Wilson <4864094+ed0906@users.noreply.github.com>
2019-06-03 10:36:55 +0100

    DIT - Dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,12 +9,12 @@ object AppDependencies {
     "org.apache.httpcomponents"   %  "httpclient"                 % httpComponentsVersion,
     "org.apache.httpcomponents"   %  "httpmime"                   % httpComponentsVersion,
     "io.megl"                     %% "play-json-extra"            % "2.4.3",
-    "uk.gov.hmrc"                 %% "bootstrap-play-25"          % "4.10.0",
+    "uk.gov.hmrc"                 %% "bootstrap-play-25"          % "4.12.0",
     "uk.gov.hmrc"                 %% "govuk-template"             % "5.31.0-play-25",
     "uk.gov.hmrc"                 %% "play-json-union-formatter"  % "1.5.0",
-    "uk.gov.hmrc"                 %% "play-reactivemongo"         % "6.2.0",
+    "uk.gov.hmrc"                 %% "play-reactivemongo"         % "6.7.0",
     "uk.gov.hmrc"                 %% "play-scheduling"            % "5.4.0",
-    "uk.gov.hmrc"                 %% "play-ui"                    % "7.38.0-play-25"
+    "uk.gov.hmrc"                 %% "play-ui"                    % "7.40.0-play-25"
   )
 
   private lazy val scope = "test, it"
@@ -27,7 +27,7 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"                % "3.0.4"                 % scope,
     "org.scalaj"              %% "scalaj-http"              % "2.4.1"                 % scope,
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"                 % scope,
-    "uk.gov.hmrc"             %% "hmrctest"                 % "3.6.0-play-25"         % scope,
+    "uk.gov.hmrc"             %% "hmrctest"                 % "3.8.0-play-25"         % scope,
     "uk.gov.hmrc"             %% "reactivemongo-test"       % "3.1.0"                 % scope,
     "uk.gov.hmrc"             %% "service-integration-test" % "0.6.0-play-25"         % scope
   )
